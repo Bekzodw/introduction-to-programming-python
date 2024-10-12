f7d2e9e61a1395470dd9a30c6cdcfb65d67b425b
BekzodShavlatov <Bekzod_Shavkatov@student.itpu.uz>
2024-10-12 16:50:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 #python
 print("I'm learning Git merge conflicts")
+name = input('Please enter your name:')
+last name = input('Please enter your last name:')
+print('Hi, {} {}!'.format(name, last name))
